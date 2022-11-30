8649604d5d7911639769b58194a2470369de61b2
LeeSeokJune <indians1720@gmail.com>
2022-11-30 17:47:57 +0900

    modify

--- lib/screens/components/custom_container_form.dart
+++ lib/screens/components/custom_container_form.dart
@@ -10,44 +10,47 @@ class CustomContainerForm extends StatelessWidget {
   var containerController = Get.put(ContainerController());
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        InkWell(
-          child: Container(
-            width: half_width * magnification,
-            height: 9 * magnification,
-            decoration: BoxDecoration(
-              border: Border.all(color: border_color),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(basic_radius),
-                topRight: Radius.circular(basic_radius),
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: border_color),
+          borderRadius: BorderRadius.circular(basic_radius)),
+      child: Column(
+        children: [
+          InkWell(
+            child: Container(
+              width: half_width * magnification,
+              height: 9 * magnification,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(basic_radius),
+                  topRight: Radius.circular(basic_radius),
+                ),
+                color: background_blue,
               ),
-              color: background_blue,
+              child: Center(
+                  child: Text(
+                '${txt}',
+                style: blue_medium_style,
+              )),
             ),
-            child: Center(
-                child: Text(
-              '${txt}',
-              style: blue_medium_style,
-            )),
+            onTap: () async {
+              if (txt == '샘플 사료 테스트' || txt == '순환 급여') {
+                containerController.set_container_index();
+              }
+            },
           ),
-          onTap: () async {
-            if (txt == '샘플 사료 테스트' || txt == '순환 급여') {
-              containerController.set_container_index();
-            }
-          },
-        ),
-        Container(
-          width: half_width * magnification,
-          decoration: BoxDecoration(
-            border: Border.all(color: border_color),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(basic_radius),
-              bottomRight: Radius.circular(basic_radius),
+          Container(
+            width: half_width * magnification,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(basic_radius),
+                bottomRight: Radius.circular(basic_radius),
+              ),
             ),
+            child: child,
           ),
-          child: child,
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
